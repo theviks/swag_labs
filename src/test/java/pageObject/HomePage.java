da42f6f3a7a4938cc6b3f3267fc6f508cdc43ebb
theviks <theviks.09@gmail.com>
2025-09-29 20:20:36 -0400

    Updating Test Case TC_P1

--- src/test/java/pageObject/HomePage.java
+++ src/test/java/pageObject/HomePage.java
@@ -1,6 +1,12 @@
 package pageObject;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
 
 public class HomePage extends BasePage {
 
@@ -8,6 +14,24 @@ public class HomePage extends BasePage {
 		super(driver);
 	}
 
+	/*-------------------------------------------------Verify All Products are displayed---------------------------------------------------*/
+	@FindBy(xpath="//div[@class='inventory_item']") List<WebElement> products;
+	
+	public void verifyAllProductsDisplayes() {
+		Assert.assertEquals(products.size(), 6);
+		
+		for(WebElement product : products) {
+			String productName = product.findElement(By.xpath("//div[@class='inventory_item_name ']")).getText();
+			String productPrice = product.findElement(By.xpath("//div[@class='inventory_item_price']")).getText();
+			WebElement image = product.findElement(By.xpath("//div[@class='inventory_item_img']"));
+			
+			Assert.assertFalse(productName.isEmpty(),"Product Name is Empty");
+			Assert.assertTrue(productPrice.startsWith("$"),"Price is Wrong");
+			Assert.assertEquals(true, image.isDisplayed());
+		}
+	}
+	
+	
 	
 	
 }
