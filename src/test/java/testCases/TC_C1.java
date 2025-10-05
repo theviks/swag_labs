d48ed74ec7db67f06f1297923d5446d3a30727a9
theviks <theviks.09@gmail.com>
2025-10-05 18:29:22 -0400

    update in code for TC_C1

--- src/test/java/testCases/TC_C1.java
+++ src/test/java/testCases/TC_C1.java
@@ -10,12 +10,27 @@ public class TC_C1 extends BaseClass {
 
 	@Test
 	public void verifyCartCount() {
+		logger.info("*** Starting Test Case: TC_C1 (Verify adding a product to cart ***");
+		try {
+			//Step1: login
 		loginPage lp = new loginPage(driver);
 		lp.enterUserName(p.getProperty("userName"));
 		lp.enterPassword(p.getProperty("password"));
 		lp.clickLogin();
+		logger.info("Login Successful with user: {}",p.getProperty("userName"));
 		
+		//Step2: Navigate to Product and add then to cart
+		logger.info("verify functionality for TC_C1: Verify addng a product to cart");
 		HomePage hp =  new HomePage(driver);
 		hp.addToCart();
+		logger.info("Cart count displayed correctly");
+		logger.info("*** Test Case: TC_C1 Passed ***");
+		}catch(Exception e) {
+			logger.error("*** Test Case: TC_C1 Failed ***");
+			logger.error("Error encountered during TC_C1 execution: {}",e.getMessage(),e);
+			throw e;
+		}finally {
+			logger.info("*** Ending Test Case: TC_C1 ***");
+		}
 	}
 }
